2baf4f0a4164b18f7d250e3383d8aa735809eaa9
姚杨 <yaoyang@yaoyangdeMacBook-Pro.local>
2018-10-20 22:53:40 +0800

    改为打war包的方式

--- src/main/java/com/yy/jenkinsdemo/JenkinsdemoApplication.java
+++ src/main/java/com/yy/jenkinsdemo/JenkinsdemoApplication.java
@@ -2,9 +2,17 @@ package com.yy.jenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsdemoApplication {
+public class JenkinsdemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JenkinsdemoApplication.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsdemoApplication.class, args);
