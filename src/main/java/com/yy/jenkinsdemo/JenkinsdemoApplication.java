e9f92d880f8bfe6f227217909d996ba92df88f03
姚杨 <yaoyang@yaoyangdeMacBook-Pro.local>
2018-10-20 23:49:51 +0800

    改回jar的方式

--- src/main/java/com/yy/jenkinsdemo/JenkinsdemoApplication.java
+++ src/main/java/com/yy/jenkinsdemo/JenkinsdemoApplication.java
@@ -2,17 +2,9 @@ package com.yy.jenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsdemoApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(JenkinsdemoApplication.class);
-    }
-
+public class JenkinsdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsdemoApplication.class, args);
